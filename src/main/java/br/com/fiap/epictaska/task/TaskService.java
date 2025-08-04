6964562568aab38741fc027668b94ba65371ad31
joaocarloslima <joaocarloslima@me.com>
2025-08-04 13:40:08 -0300

    primeira view com thymeleaf

---
+++ src/main/java/br/com/fiap/epictaska/task/TaskService.java
@@ -0,0 +1,21 @@
+package br.com.fiap.epictaska.task;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        List<Task> tasks = taskRepository.findAll();
+        return tasks;
+    }
+}
